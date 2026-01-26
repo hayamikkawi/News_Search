2a28cb98cf2d0b4983715c7d6de682b05daaf2f1
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2026-01-26 21:27:57 +0000

    code refactoring to support add documents incrementally

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,6 +1,6 @@
 import json
 from typing import TypeAlias
-
+from dataclasses import dataclass
 from config import CONFIG
 from preprocesser import preprocess_line
 
@@ -10,8 +10,17 @@ HEADLINE_KEY = "title"
 DESC_KEY = "description"
 CONTENT_KEY = "content"
 
-InvertedIndex: TypeAlias = dict[str, dict[int, set[int]]]
+# Data type for documents 
+@dataclass(frozen=True)
+class Document:
+    id: int
+    preprocessed_headline: list[str]
+    preprocessed_description: list[str]
+    preprocessed_content: list[str]
 
+InvertedIndex: TypeAlias = dict[str, dict[int, set[int]]]
+# catch the index gloabally to keep it in memory
+index: InvertedIndex = {}
 
 def write_index_to_file(token_dp: InvertedIndex, index_file: str) -> None:
     # write the data to the file
@@ -23,54 +32,53 @@ def write_index_to_file(token_dp: InvertedIndex, index_file: str) -> None:
                     f"\t{document_id}: {','.join(map(str, document_positions))}\n"
                 )
 
+def append_document_to_index(document: Document): 
+    doc_id = document.id
+    all_tokens = (
+        document.preprocessed_headline +
+        document.preprocessed_description +
+        document.preprocessed_content
+    )
+    for position, token in enumerate(all_tokens):
+        # if it appeared before in this doc, just add its pos
+        if token in index and doc_id in index[token]:
+            index[token][doc_id].add(position)
+        else:
+            if token not in index:
+                index[token] = {}
+            index[token][doc_id] = set([position])
 
-# Index creating:
-def create_index(documents: dict) -> InvertedIndex:
-    token_dp: InvertedIndex = {}
-
-    for doc_id, doc_value in documents.items():
-        all_tokens = (
-            doc_value[HEADLINE_KEY] + doc_value[DESC_KEY] + doc_value[CONTENT_KEY]
-        )
-        for position, token in enumerate(all_tokens):
-            # if it appeared before in this doc, just add its pos
-            if token in token_dp and doc_id in token_dp[token]:
-                token_dp[token][doc_id].add(position)
-            else:
-                if token not in token_dp:
-                    token_dp[token] = {}
-                token_dp[token][doc_id] = set([position])
-
-    return token_dp
-
+def preprocess_document(document: dict) -> Document: 
+    processed_headline = preprocess_line(document[HEADLINE_KEY])
+    processed_desc = preprocess_line(document[DESC_KEY])
+    processed_content = preprocess_line(document[CONTENT_KEY])
+    return Document(document[ID_KEY], processed_headline,
+                    processed_desc, processed_content)
 
 def indexing_main(input: str, output: str) -> None:
-    documents_output = {}
-
     # TODO: parse the files into docs
     with open(input, "r", encoding="utf-8") as f:
         documents: list[dict] = json.load(f)
-
-    # preprocess_line the text
+    # preprocess each document and save the result in Document object,
+    # then add the document to the index
     for document in documents:
-        doc_id = document[ID_KEY]
-        documents_output[doc_id] = {}
-        processed_headline = preprocess_line(document[HEADLINE_KEY])
-        processed_desc = preprocess_line(document[DESC_KEY])
-        processed_content = preprocess_line(document[CONTENT_KEY])
-        documents_output[doc_id][HEADLINE_KEY] = processed_headline
-        documents_output[doc_id][DESC_KEY] = processed_desc
-        documents_output[doc_id][CONTENT_KEY] = processed_content
-
-    # index the documents
-    index = create_index(documents_output)
+        processed_document = preprocess_document(document)
+        append_document_to_index(processed_document)
+    # write the result to output file
     write_index_to_file(index, output)
 
+# This will be called from outside to add more documents
+def add_new_documents(documents: list[dict]) -> None:
+    for document in documents: 
+        add_new_document(document)
+
+def add_new_document(document: dict) -> None:
+    preprocessed_document = preprocess_document(document)
+    append_document_to_index(preprocessed_document)
 
 def main() -> None:
     input_file = CONFIG.input_file_path
     output_file = CONFIG.output_file_path
-
     indexing_main(input_file, output_file)
 
 
