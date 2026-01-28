f728b3dbce70d0386ea0737d5f615f8d37bcf920
Aidan Maney <s2891640@ed.ac.uk>
2026-01-28 00:04:46 +0000

    Start on binary index.

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,6 +1,9 @@
 import json
-from typing import TypeAlias
+import mmap
+import struct
 from dataclasses import dataclass
+from typing import TypeAlias
+
 from config import CONFIG
 from preprocesser import preprocess_line
 
@@ -10,7 +13,8 @@ HEADLINE_KEY = "title"
 DESC_KEY = "description"
 CONTENT_KEY = "content"
 
-# Data type for documents 
+
+# Data type for documents
 @dataclass(frozen=True)
 class Document:
     id: int
@@ -18,9 +22,11 @@ class Document:
     preprocessed_description: list[str]
     preprocessed_content: list[str]
 
+
 InvertedIndex: TypeAlias = dict[str, dict[int, set[int]]]
 # catch the index gloabally to keep it in memory
 index: InvertedIndex = {}
+
 
 def write_index_to_file(index_file: str) -> None:
     # write the data to the file
@@ -32,12 +38,42 @@ def write_index_to_file(index_file: str) -> None:
                     f"\t{document_id}: {','.join(map(str, document_positions))}\n"
                 )
 
-def append_document_to_index(document: Document): 
+
+def write_index_to_binary_file(index_file: str) -> None:
+    def compute_posting_deltas(sorted_postings: list[int]):
+        posting_deltas = []
+
+        last_posting = 0
+        for posting in sorted_postings:
+            posting_deltas.append(posting - last_posting)
+            last_posting = posting
+
+        return posting_deltas
+
+    with open(index_file, "wb") as index_output_file:
+        for token in list(index.keys())[:1]:
+            n_tokens = len(index.keys())
+            # Q -> 8 bytes (unsigned long long)
+            index_output_file.write(struct.pack("@Q", n_tokens))
+            break
+
+
+def read_index_from_binary_file(index_path: str) -> InvertedIndex:
+    # Need to open with '+' otherwise permission denied
+    with open(index_path, "rb+") as index_file:
+        with mmap.mmap(index_file.fileno(), 0) as mm:
+            n_terms = struct.unpack("@Q", mm[0:8])[0]
+            print(n_terms)
+
+    return dict()
+
+
+def append_document_to_index(document: Document):
     doc_id = document.id
     all_tokens = (
-        document.preprocessed_headline +
-        document.preprocessed_description +
-        document.preprocessed_content
+        document.preprocessed_headline
+        + document.preprocessed_description
+        + document.preprocessed_content
     )
     for position, token in enumerate(all_tokens):
         # if it appeared before in this doc, just add its pos
@@ -48,12 +84,15 @@ def append_document_to_index(document: Document):
                 index[token] = {}
             index[token][doc_id] = set([position])
 
-def preprocess_document(document: dict) -> Document: 
+
+def preprocess_document(document: dict) -> Document:
     processed_headline = preprocess_line(document[HEADLINE_KEY])
     processed_desc = preprocess_line(document[DESC_KEY])
     processed_content = preprocess_line(document[CONTENT_KEY])
-    return Document(document[ID_KEY], processed_headline,
-                    processed_desc, processed_content)
+    return Document(
+        document[ID_KEY], processed_headline, processed_desc, processed_content
+    )
+
 
 def indexing_main(input: str, output: str) -> None:
     # TODO: parse the files into docs
@@ -65,16 +104,20 @@ def indexing_main(input: str, output: str) -> None:
         processed_document = preprocess_document(document)
         append_document_to_index(processed_document)
     # write the result to output file
-    write_index_to_file(output)
+    write_index_to_binary_file(output)
+    read_index_from_binary_file(output)
+
 
 # This will be called from outside to add more documents
 def add_new_documents(documents: list[dict]) -> None:
-    for document in documents: 
+    for document in documents:
         add_new_document(document)
+
 
 def add_new_document(document: dict) -> None:
     preprocessed_document = preprocess_document(document)
     append_document_to_index(preprocessed_document)
+
 
 def main() -> None:
     input_file = CONFIG.input_file_path
