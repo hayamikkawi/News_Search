5d2572b7bb60419fbc46b9d07e2c891e974af3a5
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2026-01-31 14:55:17 +0000

    add preprocess step

--- IR/free_text_query_handler.py
+++ IR/free_text_query_handler.py
@@ -2,5 +2,6 @@ from query_handler import QueryHandler
 
 class FreeTextQueryHandler(QueryHandler): 
     def handle_query(query: str) -> list[str]:
-        # TODO: implement
+        # preprocess the query into tokens
+        query_tokens: list [str]
         return []
